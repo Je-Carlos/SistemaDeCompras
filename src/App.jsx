62a8668606ade08282cf20f354993fb564e28e43
Jean Carlos <dev.jeancotillo@gmail.com>
2024-08-28 16:49:47 -0300

    fix-arrumando rota login

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <div className="bg-gray-900 text-white min-h-screen">
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
           <Route path="*" element={<MainLayout />} />
         </Routes>
       </BrowserRouter>
