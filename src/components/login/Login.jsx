00fe1ca8d02d07cd87af478d40c50158954b1547
Jean Carlos <dev.jeancotillo@gmail.com>
2024-09-13 17:58:10 -0300

    atualizando tela de login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { auth } from "../../firebase/firebaseConfig"; // Certifique-se de ajustar o caminho conforme necessário
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/firebaseConfig";
+import "./login.css";
 
-const Login = () => {
+export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -11,18 +12,17 @@ const Login = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    console.log("Tentando fazer login com:", username, password); // Log para depuração
     try {
       const userCredential = await signInWithEmailAndPassword(
         auth,
         username,
         password
       );
-      console.log("Login bem-sucedido:", userCredential); // Log para depuração
+      console.log("Login bem-sucedido:", userCredential);
       localStorage.setItem("isAuthenticated", "true");
       navigate("/home");
     } catch (error) {
-      console.error("Erro durante o login:", error); // Log para depuração
+      console.error("Erro durante o login:", error);
       setError("Usuário ou senha inválidos");
     }
   };
@@ -30,35 +30,39 @@ const Login = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900">
       <div className="bg-gray-800 p-8 rounded-lg shadow-md w-96">
+        <h1 className="text-4xl font-extrabold mb-2 text-center text-purple-500 animate-color-change">
+          SISCOMP
+        </h1>
+        <p className="text-lg font-medium mb-6 text-center text-gray-400">
+          Sistema de Compras Inteligente
+        </p>
         <h2 className="text-2xl font-bold mb-6 text-center text-white">
           Login
         </h2>
         <form onSubmit={handleLogin} className="space-y-4">
           <input
             type="email"
+            placeholder="Email"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            placeholder="Email"
-            className="w-full px-4 py-2 border border-gray-600 bg-gray-700 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-white"
+            className="w-full p-2 bg-gray-700 text-white border border-gray-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
           />
           <input
             type="password"
+            placeholder="Senha"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            className="w-full px-4 py-2 border border-gray-600 bg-gray-700 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-white"
+            className="w-full p-2 bg-gray-700 text-white border border-gray-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
           />
+          {error && <p className="text-red-500 text-sm">{error}</p>}
           <button
             type="submit"
-            className="w-full bg-purple-500 text-white py-2 rounded-md hover:bg-purple-700 transition duration-300"
+            className="w-full p-2 bg-purple-600 text-white rounded hover:bg-purple-700 transition duration-300"
           >
-            Login
+            Entrar
           </button>
         </form>
-        {error && <p className="text-red-500 mt-4 text-center">{error}</p>}
       </div>
     </div>
   );
-};
-
-export default Login;
+}
